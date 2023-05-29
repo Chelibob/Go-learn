abfea6ee369579d9b88e708b5d2e8f8489e28162
Chelibob <nicekolya.a7x@gmail.com>
2023-05-29 21:58:33 +0500

    Изменил методы по выводу и вычислению радиуса круга, использовав указатели

--- Chapt01/main.go
+++ Chapt01/main.go
@@ -7,24 +7,25 @@ import (
 )
 
 func main() {
-	printCircleArea(2)
+	radius := 2
+	printCircleArea(&radius)
 }
 
-func printCircleArea(radius int) {
+func printCircleArea(radius *int) {
 	circleArea, err := calcCircleArea(radius)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	fmt.Printf("Радиус круга: %d см.\n", radius)
+	fmt.Printf("Радиус круга: %d см.\n", *radius)
 	fmt.Println("Формула для расчета площади круга: S=πr2\n")
 
 	fmt.Printf("Площадь круга: %f32 см. кв.\n\n", circleArea)
 }
 
-func calcCircleArea(radius int) (float32, error) {
-	if radius <= 0 {
+func calcCircleArea(radius *int) (float32, error) {
+	if *radius <= 0 {
 		return 0.0, errors.New("Радиус не может быть меньше или равен нулю")
 	}
-	return float32(radius) * float32(radius) * math.Pi, nil
+	return float32(*radius) * float32(*radius) * math.Pi, nil
 }
